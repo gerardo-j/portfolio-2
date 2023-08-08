9d77c2cfad44943c90641fe22c6dff66147c4705
Gerardo <gerardojuarez695@gmail.com>
2023-08-07 22:52:39 -0400

    Updated root page
    Added basic contact info

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<body>
+	<div>
+		<slot />
+	</div>
+</body>
+
+<style>
+	body {
+		margin: 0;
+		padding: 0;
+		font-family: sans-serif;
+		color: #023047;
+	}
+
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		background-color: #ffb703;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="contact-info">
+	<h2 class="sub-head">🔧 Under construction 🔧</h2>
+
+	<p>👋 Thanks for stopping by! Here is my personal portfolio.</p>
+
+	<p>🔗 Live site <a href="https://meetgerardo.com">https://meetgerardo.com</a></p>
+	<p>📩 Email: <a href="mailto:gerardojuarez695@gmail.com">gerardojuarez695@gmail.com</a></p>
+	<p>
+		⚙️ Linkedin: <a href="https://www.linkedin.com/in/gerardo-juarez-cano/"
+			>https://www.linkedin.com/in/gerardo-juarez-cano/</a
+		>
+	</p>
+</div>
+
+<style>
+	.sub-head {
+		color: #023047;
+		text-align: center;
+	}
+	.contact-info {
+		background-color: rgba(128, 128, 128, 0.8);
+		padding: 1rem;
+	}
+</style>
