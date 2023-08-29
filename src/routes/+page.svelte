ed2ea38016009ce73bb9501e93990f1b4239efa5
Gerardo <gerardojuarez695@gmail.com>
2023-08-28 20:53:44 -0400

    Update Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<div class="contact-info">
+<script>
+	const exampleData = 'This text was copied by the Skeleton clipboard action.';
+</script>
+
+<svelte:head>
+	<title>Hello</title>
+</svelte:head>
+<div class="contact-info grid">
 	<h2 class="sub-head">🔧 Under construction 🔧</h2>
 
 	<p>👋 Thanks for stopping by! Here is my personal portfolio.</p>
@@ -10,15 +17,24 @@
 			>https://www.linkedin.com/in/gerardo-juarez-cano/</a
 		>
 	</p>
+
+	<button type="button" class="btn btn-sm variant-filled m-auto"
+		><a href="https://gerardo-j.github.io/portfolio/">View previous portfolio</a></button
+	>
 </div>
 
 <style>
 	.sub-head {
 		color: #023047;
+		font-weight: bold;
 		text-align: center;
 	}
 	.contact-info {
 		background-color: rgba(128, 128, 128, 0.8);
-		padding: 1rem;
+		padding: 2rem;
+	}
+
+	.contact-info p {
+		margin: 0.25rem 0;
 	}
 </style>
