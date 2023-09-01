41595627f65cbc971237634df48ce60a78d9d53a
Gerardo <gerardojuarez695@gmail.com>
2023-09-01 19:55:50 -0400

    Update style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,24 +3,5 @@
 </script>
 
 <body>
-	<!-- <div> -->
 	<slot />
-	<!-- </div> -->
 </body>
-
-<style>
-	body {
-		/* margin: 0; */
-		/* padding: 0; */
-		/* color: #023047; */
-	}
-
-	div {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		height: 100vh;
-		background-color: #ffb703;
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,25 @@
-<script>
-	const exampleData = 'This text was copied by the Skeleton clipboard action.';
-</script>
-
 <svelte:head>
-	<title>Hello</title>
+	<title>Under Construction</title>
 </svelte:head>
-<div class="contact-info grid">
-	<h2 class="sub-head">🔧 Under construction 🔧</h2>
+
+<div class="rounded-token grid bg-primary-200 p-4 max-w-lg mx-auto mt-8">
+	<h2 class="h2 font-bold text-center">🔧 Under construction 🔧</h2>
 
 	<p>👋 Thanks for stopping by! Here is my personal portfolio.</p>
 
-	<p>🔗 Live site <a href="https://meetgerardo.com">https://meetgerardo.com</a></p>
-	<p>📩 Email: <a href="mailto:gerardojuarez695@gmail.com">gerardojuarez695@gmail.com</a></p>
+	<p>🔗 Live site <a class="anchor" href="https://meetgerardo.com">https://meetgerardo.com</a></p>
 	<p>
-		⚙️ Linkedin: <a href="https://www.linkedin.com/in/gerardo-juarez-cano/"
+		📩 Email: <a class="anchor" href="mailto:gerardojuarez695@gmail.com"
+			>gerardojuarez695@gmail.com</a
+		>
+	</p>
+	<p>
+		⚙️ Linkedin: <a class="anchor" href="https://www.linkedin.com/in/gerardo-juarez-cano/"
 			>https://www.linkedin.com/in/gerardo-juarez-cano/</a
 		>
 	</p>
 
-	<button type="button" class="btn btn-sm variant-filled m-auto"
+	<button type="button" class="btn btn-sm variant-filled-primary m-auto"
 		><a href="https://gerardo-j.github.io/portfolio/">View previous portfolio</a></button
 	>
 </div>
-
-<style>
-	.sub-head {
-		color: #023047;
-		font-weight: bold;
-		text-align: center;
-	}
-	.contact-info {
-		background-color: rgba(128, 128, 128, 0.8);
-		padding: 2rem;
-	}
-
-	.contact-info p {
-		margin: 0.25rem 0;
-	}
-</style>
